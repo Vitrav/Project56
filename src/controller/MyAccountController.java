3f0c0dcb4f3f1070e40b2976572452c128ecfa65
MikeKouwenhoven <mkouwenhoven12@gmail.com>
2016-11-04 13:59:11 +0100

    MyAccount weergeeft goede info, wil alleen css niet pakken

--- src/controller/MyAccountController.java
+++ src/controller/MyAccountController.java
@@ -1,19 +1,29 @@
 package controller;
 
+import model.collection.UserCollectionManager;
+import model.document.UserDocumentManager;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import user.UserController;
 import viewutil.Path;
 import viewutil.ViewUtil;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static viewutil.RequestUtil.getEmail;
+import static viewutil.RequestUtil.getQueryUsername;
+import static viewutil.RequestUtil.getSessionCurrentUser;
+
 public class MyAccountController {
 
     public static Route accountPage = (Request request, Response response) -> {
         Map<String, Object> model = new HashMap<>();
+        UserCollectionManager userCollectionManager = new UserCollectionManager();
+        UserDocumentManager userDocumentManager = new UserDocumentManager(userCollectionManager.getUserDocument(getSessionCurrentUser(request)));
+
+        model.put("userInfo", userDocumentManager);
         return ViewUtil.render(request, model, Path.Template.MYACCOUNT);
     };
-
 }
