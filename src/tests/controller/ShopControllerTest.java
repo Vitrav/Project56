ead65f833d63fb1c8d7f744c88b8dd52a2d603b9
Sivar72 <sivarwerrie@hotmail.nl>
2017-01-27 11:44:13 +0100

    Tests uncommented.

--- src/tests/controller/ShopControllerTest.java
+++ src/tests/controller/ShopControllerTest.java
@@ -12,29 +12,29 @@ import static org.junit.Assert.*;
 //Test the cart logic when not logged in
 public class ShopControllerTest {
 
-//    private final String testUser = "user";
-//
-//    @Test
-//    public void removeGameFromUser() throws Exception {
-//        ShopController.getUserItems().clear();
-//        assertFalse(ShopController.getUserItems().containsKey(testUser));
-//        ShopController.getUserItems().put(testUser, new LinkedList<>(Arrays.asList(new UserCollectionManager().createCartItemDocument(1))));
-//        assertTrue(ShopController.getUserItems().containsKey(testUser));
-//        assertTrue(ShopController.userHasGame(testUser, 1));
-//        assertTrue(ShopController.getUserItems().get(testUser).get(0).getInteger("id") == 1);
-//        ShopController.removeUserItem(testUser, 1);
-//        assertFalse(ShopController.userHasGame(testUser, 1));
-//    }
-//
-//    @Test
-//    public void incGameAmount() throws Exception {
-//        ShopController.getUserItems().put(testUser, new LinkedList<>(Arrays.asList(new UserCollectionManager().createCartItemDocument(1))));
-//        assertTrue(ShopController.getUserItems().get(testUser).get(0).getInteger("amount") == 1);
-//        ShopController.incGameAmount(testUser, 1);
-//        assertTrue(ShopController.getUserItems().get(testUser).get(0).getInteger("amount") == 2);
-//        ShopController.incGameAmount(testUser, 2);
-//        assertTrue(ShopController.getUserItems().get(testUser).get(0).getInteger("amount") == 2);
-//    }
+    private final String testUser = "user";
+
+    @Test
+    public void removeGameFromUser() throws Exception {
+        ShopController.getUserItems().clear();
+        assertFalse(ShopController.getUserItems().containsKey(testUser));
+        ShopController.getUserItems().put(testUser, new LinkedList<>(Arrays.asList(new UserCollectionManager().createCartItemDocument(1))));
+        assertTrue(ShopController.getUserItems().containsKey(testUser));
+        assertTrue(ShopController.userHasGame(testUser, 1));
+        assertTrue(ShopController.getUserItems().get(testUser).get(0).getInteger("id") == 1);
+        ShopController.removeUserItem(testUser, 1);
+        assertFalse(ShopController.userHasGame(testUser, 1));
+    }
+
+    @Test
+    public void incGameAmount() throws Exception {
+        ShopController.getUserItems().put(testUser, new LinkedList<>(Arrays.asList(new UserCollectionManager().createCartItemDocument(1))));
+        assertTrue(ShopController.getUserItems().get(testUser).get(0).getInteger("amount") == 1);
+        ShopController.incGameAmount(testUser, 1);
+        assertTrue(ShopController.getUserItems().get(testUser).get(0).getInteger("amount") == 2);
+        ShopController.incGameAmount(testUser, 2);
+        assertTrue(ShopController.getUserItems().get(testUser).get(0).getInteger("amount") == 2);
+    }
 
 
 }
