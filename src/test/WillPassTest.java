18a77ebaaae4127081b4e11f78ccd37ffdc372d6
Sivar72 <sivarwerrie@hotmail.nl>
2017-01-30 13:09:56 +0100

    Junit test fixed.

---
+++ src/test/WillPassTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class WillPassTest {
+
+    @org.junit.Test
+    public void willPass() throws Exception {
+       assertTrue(5 != 6);
+    }
+
+
+}
